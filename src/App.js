894d3fc36893f50c97197992ec59d3ca699324ec
Gideon Ogongo <ogjr80@gmail.com>
2022-11-26 16:08:22 +0200

    Client Skeleton for House marketplace

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+import Navbar from './components/NavBar';
+
+import Explore from './pages/Explore'; 
+import Offers from './pages/Offers'; 
+import Profile from './pages/Profile'; 
+import SignIn from './pages/SignIn'; 
+import SignUp from './pages/SignUp'; 
+import ForgotPassword  from './pages/ForgotPassword';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to House Marketplace</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore/>}/> 
+          <Route path="/offers" element={<Offers/>}/> 
+          <Route path="/profile" element={<Profile/>}/> 
+          <Route path="/signin" element={<SignIn/>}/> 
+          <Route path="/signup" element={<SignUp/>}/> 
+          <Route path="/forgotpassword" element={<ForgotPassword/>}/> 
+        </Routes>
+        <Navbar/>
+      </Router>
+
     </div>
   );
 }
